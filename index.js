7bc100d0ebf3ce6304ee828c908fcc788708200d
Emily Cubbage <emilycubbage@gmail.com>
2018-06-24 17:57:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function loadIssue() {
  
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
-  var result = template(issue);
+  for(var i=0;i<issues.length;i++) {
+    var result = template(issues[i]);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
